0cf10b3a846abcc0ce5b9da1d1b91b80750987dc
battleheart70 <battleheart70@gmail.com>
2025-01-12 21:29:41 +0100

    Connected telegram bot. Removed sensitive info into config file.

--- TinderBolt/src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ TinderBolt/src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -9,12 +9,30 @@ import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
-    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: добавь токен бота в кавычках
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+    public static final String TELEGRAM_BOT_NAME;
+    public static final String TELEGRAM_BOT_TOKEN;
+    public static final String OPEN_AI_TOKEN;
+
+    static {
+        Properties properties = new Properties();
+        try (InputStream input = TinderBoltApp.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Sorry, unable to find config.properties");
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            throw new RuntimeException("Error loading config.properties", ex);
+        }
+        TELEGRAM_BOT_NAME = properties.getProperty("TELEGRAM_BOT_NAME");
+        TELEGRAM_BOT_TOKEN = properties.getProperty("TELEGRAM_BOT_TOKEN");
+        OPEN_AI_TOKEN = properties.getProperty("OPEN_AI_TOKEN");
+    }
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -23,6 +41,21 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         //TODO: основной функционал бота будем писать здесь
+        String message = getMessageText();
+        if (message.equals("/start")) {
+            sendPhotoMessage("main");
+            sendTextMessage(loadMessage("main"));
+            return;
+        }
+
+        sendTextMessage("*Привет, ты мой бот!*");
+        sendTextMessage("_Привет, ты мой бот!_");
+
+        sendTextMessage("Ты написал: " + message);
+
+        sendTextButtonsMessage("Выбери режим работы", "Start", "Start",
+                "Stop", "Stop");
+
 
     }
 
